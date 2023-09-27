6adaaca684d4182b0af7ef37c278d413f550f94f
henrietta jeansson <henriettajeansson@gmail.com>
2023-09-27 09:17:46 +0200

    add partials styling header

--- themes/artshop-theme/header.php
+++ themes/artshop-theme/header.php
@@ -8,15 +8,16 @@
     <title><?php bloginfo("name"); ?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
     <?php wp_head() ?>
-    <script src="<?php echo get_template_directory_uri() . '/main.js'?>" defer type="text/javascript" ></script>
+    <!-- <script src="<?php echo get_template_directory_uri() . '/main.js' ?>" defer type="text/javascript"></script> -->
 </head>
 
 <body>
     <header>
-    <h1>
-        <?php bloginfo("name"); ?>
-    </h1>
-    <?php get_template_part("includes/section", "navigation") ?>
+        <h1>
+            <?php bloginfo("name"); ?>
+        </h1>
+
+        <?php get_template_part("includes/section", "navigation") ?>
 
     </header>
     <main>
