d1a54d70ced163b0812b367c390f2f5cc5d5ad2b
henrietta jeansson <henriettajeansson@gmail.com>
2023-09-27 14:49:01 +0200

    add some styling search page

--- themes/artshop-theme/search.php
+++ themes/artshop-theme/search.php
@@ -5,30 +5,25 @@
  <?php
     get_header(); ?>
 
- <section class="custom-wrapper">
-     <section class="content-container">
+ <section class="content-container">
 
-         <h1 class="page-title">Sökresultat för: <?php echo get_search_query(); ?></h1>
-         <?php
-            if (have_posts()) : while (have_posts()) : the_post();
+     <h1 class="page-title">Sökresultat för: <?php echo get_search_query(); ?></h1>
+     <?php
+        if (have_posts()) : while (have_posts()) : the_post();
 
-            ?>
-                 <article>
-                     <h2><a href="<?php the_permalink(); ?>"><?php
-                                                                the_title();
-                                                                ?> </a>
-                     </h2>
-                     <div id="img-container">
-                         <?php the_post_thumbnail() ?>
-                     </div>
-                 </article>
-         <?php
+        ?>
+             <article class="content-container__box">
+                 <h2><a href="<?php the_permalink(); ?>"><?php
+                                                            the_title();
+                                                            ?> </a>
+                 </h2>
+                 <?php the_post_thumbnail() ?>
+             </article>
+     <?php
 
-                endwhile;
-            endif;
-            ?>
-     </section>
-
+            endwhile;
+        endif;
+        ?>
  </section>
  <?php
     get_search_feed_link();
