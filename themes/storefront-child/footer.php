c69e3c60c48328cfe932deced321348ba597980c
sergejsvoronins <svoronins@gmail.com>
2023-09-29 14:16:25 +0200

    footer is fixed

--- themes/storefront-child/footer.php
+++ themes/storefront-child/footer.php
@@ -1,21 +1,41 @@
-</main>
 
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package storefront
+ */
 
-<footer id="footer" role="contentinfo">
-<section class="footer_wrapper">
-        <article class="about">
-            <?php dynamic_sidebar('footer-4'); ?>
-        </article>
-        <article class="social">
-            <?php dynamic_sidebar('footer-3'); ?>
-        </article>
-        <article class="copyrights">
-            <?php dynamic_sidebar('footer-1'); ?>
-        </article>
-    </section>
-</footer>
-</div>
+?>
+
+		</div><!-- .col-full -->
+	</div><!-- #content -->
+
+	<?php do_action( 'storefront_before_footer' ); ?>
+
+	<footer id="colophon footer" class="site-footer" role="contentinfo">
+		<div class="col-full">
+			<?php
+			/**
+			 * Functions hooked in to storefront_footer action
+			 *
+			 * @hooked storefront_footer_widgets - 10
+			 * @hooked storefront_credit         - 20
+			 */
+			do_action( 'storefront_footer' );
+			?>
+
+		</div><!-- .col-full -->
+	</footer><!-- #colophon -->
+
+	<?php do_action( 'storefront_after_footer' ); ?>
+
+</div><!-- #page -->
+
 <?php wp_footer(); ?>
-</body>
 
+</body>
 </html>
+
--- themes/storefront-child/front-page.php
+++ themes/storefront-child/front-page.php
@@ -4,8 +4,9 @@
     $homeHeader = get_field("home_header");
     $homeTextContent = get_field("home_text_content");
     $homeButton = get_field("home_button") ?>
-
-    <section class="home-page">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+        <section class="home-page">
         <?php 
             if ( have_posts() ) :
                 while ( have_posts() ) : the_post();?>
@@ -33,4 +34,22 @@
         ?>
 
         </section>
+			<?php
+			/**
+			 * Functions hooked in to homepage action
+			 *
+			 * @hooked storefront_homepage_content      - 10
+			 * @hooked storefront_product_categories    - 20
+			 * @hooked storefront_recent_products       - 30
+			 * @hooked storefront_featured_products     - 40
+			 * @hooked storefront_popular_products      - 50
+			 * @hooked storefront_on_sale_products      - 60
+			 * @hooked storefront_best_selling_products - 70
+			 */
+			do_action( 'homepage' );
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
 <?php get_footer() ?>
