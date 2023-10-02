81bebecbe4dda33dbbae19de4e8e1078534e7ff8
henrietta jeansson <henriettajeansson@gmail.com>
2023-10-02 14:21:20 +0200

    add touches to email templates

--- themes/storefront-child/emails/email-header.php
+++ themes/storefront-child/emails/email-header.php
@@ -30,7 +30,7 @@ if (!defined('ABSPATH')) {
     <title style="font-family: Optima, sans-serif" color="#dcbbbb"><?php echo get_bloginfo('name', 'display'); ?></title>
 </head>
 
-<body style="font-family: Helvetica, sans-serif; line-height:125%; color:#663704; " <?php echo is_rtl() ? 'rightmargin' : 'leftmargin'; ?>="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
+<body style="font-family: Helvetica, sans-serif; line-height:125%; color:#663704; text-decoration:none;" <?php echo is_rtl() ? 'rightmargin' : 'leftmargin'; ?>="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
     <table width="100%" id="outer_wrapper">
         <tr>
             <td><!-- Deliberately empty to support consistent sizing and layout across multiple email clients. --></td>
@@ -55,7 +55,7 @@ if (!defined('ABSPATH')) {
                                             <table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_header">
                                                 <tr>
                                                     <td id="header_wrapper">
-                                                        <h1 style="font-family: Optima, sans serif; color:#6b0a00; text-align:center;"><?php echo esc_html($email_heading); ?></h1>
+                                                        <h1 style="font-family: Optima, sans serif; text-align:center;"><?php echo esc_html($email_heading); ?></h1>
                                                     </td>
                                                 </tr>
                                             </table>
