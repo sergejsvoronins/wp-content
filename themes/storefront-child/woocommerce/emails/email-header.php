ddbc93fc6d65042a41d04a6f6e47ba1e267789e4
henrietta jeansson <henriettajeansson@gmail.com>
2023-10-02 12:58:30 +0200

    add fonts to email header

--- themes/storefront-child/woocommerce/emails/email-header.php
+++ themes/storefront-child/woocommerce/emails/email-header.php
@@ -27,14 +27,14 @@ if (!defined('ABSPATH')) {
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset'); ?>" />
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title font-family="Tahoma, sans serif" color="#dcbbbb"><?php echo get_bloginfo('name', 'display'); ?></title>
+    <title style="font-family: Optima, sans-serif" color="#dcbbbb"><?php echo get_bloginfo('name', 'display'); ?></title>
 </head>
 
-<body <?php echo is_rtl() ? 'rightmargin' : 'leftmargin'; ?>="0" font-family="Garamond, sans-serif" marginwidth="0" topmargin="0" marginheight="0" offset="0">
+<body style="font-family: Helvetica, sans-serif; line-height:125%; color:#663704; " <?php echo is_rtl() ? 'rightmargin' : 'leftmargin'; ?>="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
     <table width="100%" id="outer_wrapper">
         <tr>
             <td><!-- Deliberately empty to support consistent sizing and layout across multiple email clients. --></td>
-            <td width="600">
+            <td width="600" height="300">
                 <div id="wrapper" dir="<?php echo is_rtl() ? 'rtl' : 'ltr'; ?>">
                     <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
                         <tr>
@@ -55,7 +55,7 @@ if (!defined('ABSPATH')) {
                                             <table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_header">
                                                 <tr>
                                                     <td id="header_wrapper">
-                                                        <h1 style="font-family: 'Garamond', serif"><?php echo esc_html($email_heading); ?></h1>
+                                                        <h1 style="font-family: Optima, sans serif; color:#6b0a00; text-align:center;"><?php echo esc_html($email_heading); ?></h1>
                                                     </td>
                                                 </tr>
                                             </table>
