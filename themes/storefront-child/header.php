06299d0b97810260f422557d792a5a729936e526
henrietta jeansson <henriettajeansson@gmail.com>
2023-10-05 14:57:02 +0200

    add new large header image and styling mobile navbar

--- themes/storefront-child/header.php
+++ themes/storefront-child/header.php
@@ -20,14 +20,11 @@
     <div id="page" class="hfeed site">
         <?php do_action('storefront_before_header'); ?>
 
-        <header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
+        <header id="masthead" class="site-header mobile-header" role="banner" style="<?php storefront_header_styles(); ?>">
 
             <?php
             do_action('storefront_header');
             ?>
-            <!-- <img class="img-icon" src="<?php get_template_directory_uri() ?>/e-commerce/wp-content/themes/storefront-child/assets/sun.svg" alt="Geometrisk form"> -->
-
-
             <?php
             do_action('storefront_before_content');
             ?>
