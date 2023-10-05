0f702d6e8ccfef67b1371f59ff04b7d8daa58773
henrietta jeansson <henriettajeansson@gmail.com>
2023-10-05 19:02:14 +0200

    add style to front page button

--- themes/storefront-child/header.php
+++ themes/storefront-child/header.php
@@ -20,7 +20,7 @@
     <div id="page" class="hfeed site">
         <?php do_action('storefront_before_header'); ?>
 
-        <header id="masthead" class="site-header mobile-header" role="banner" style="<?php storefront_header_styles(); ?>">
+        <header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
 
             <?php
             do_action('storefront_header');
