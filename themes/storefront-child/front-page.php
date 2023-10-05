242054d96468e5a9b32cd5658e1260ec41767233
henrietta jeansson <henriettajeansson@gmail.com>
2023-10-05 18:51:44 +0200

    add styling to first welcome acf frontpage

--- themes/storefront-child/front-page.php
+++ themes/storefront-child/front-page.php
@@ -12,7 +12,7 @@
  *
  * @package storefront
  */
-$heroImage = get_field("home_image");
+$heroImage = get_field("hero_image");
 $homeHeader = get_field("home_header");
 $homeTextContent = get_field("home_text_content");
 $homeButton = get_field("home_button");
@@ -21,16 +21,17 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
-        <section class="home-page">
-            <?php
-            if (have_posts()) :
-                while (have_posts()) : the_post(); ?>
-                    <article class="hero-image">
+
+        <?php
+        if (have_posts()) :
+            while (have_posts()) : the_post(); ?>
+                <section class="home-page">
+                    <article class="home-page__image">
                         <?php if ($heroImage) : ?>
                             <img src="<?php echo $heroImage["url"] ?>" alt="<?php echo $heroImage["alt"] ?>">
                         <?php endif; ?>
                     </article>
-                    <article class="text">
+                    <article class="home-page__text">
                         <?php if ($homeHeader) : ?>
                             <h2><?php the_field("home_header") ?></h2>
                         <?php endif; ?>
@@ -43,12 +44,13 @@ get_header(); ?>
                             </a>
                         <?php endif; ?>
                     </article>
-            <?php
-                endwhile;
-            endif;
-            ?>
+                </section>
+        <?php
+            endwhile;
+        endif;
+        ?>
 
-        </section>
+
     </main>
 
     <?php
